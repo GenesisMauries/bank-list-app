a80820729e70fc862b259724dd736a7ccfac94fc
Genesis Mauries <exodo_54@hotmail.com>
2024-06-20 11:20:02 -0600

    add new api config

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,41 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
-        port: '',
-        pathname: '**',
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
       },
     ],
+  },
+  // next.config.mjs
+
+  async headers() {
+    return [
+      {
+        // Apply headers to all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://deploy-bank-list-app-git-new-styles-genesis-projects-e622ce38.vercel.app/",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
