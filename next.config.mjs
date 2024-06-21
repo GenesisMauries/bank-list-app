a2a868f7c58a7b142a3f9a6fb353b0e33884cae4
Genesis Mauries <exodo_54@hotmail.com>
2024-06-21 09:52:57 -0600

    delete async headers in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,34 +10,7 @@ const nextConfig = {
         pathname: "**",
       },
     ],
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://deploy-bank-list-app.vercel.app/",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept- Version, Content - Length, Content - MD5, Content - Type, Date, X - Api - Version",
-          },
-        ],
-      },
-    ];
-  },
+  }
 };
 
 export default nextConfig;
