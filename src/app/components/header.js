4d772288dea2df07394224e10c02408b27605bc9
Genesis Mauries <exodo_54@hotmail.com>
2024-06-25 08:59:35 -0600

    add sandbox img vs Image

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -4,7 +4,14 @@ export default function Header() {
   return (
     <header>
       <section className={styles.triangle}></section>
-      <Image
+      {/* <Image
+        src="https://github.com/GenesisMauries/bank-list-app/assets/39414582/f24b0b07-edfd-4d84-9de8-10f81c175146"
+        width={35}
+        height={35}
+        alt= "Telefono icon"
+      /> */}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
         src="https://github.com/GenesisMauries/bank-list-app/assets/39414582/f24b0b07-edfd-4d84-9de8-10f81c175146"
         width={35}
         height={35}
